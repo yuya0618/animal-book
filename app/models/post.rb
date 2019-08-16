b8f10e1e788f224c7f9f927f5c97d444ec64804e
yuya0618 <yuya.aki.0618@gmail.com>
2019-08-16 19:31:57 +0900

    editDelete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :likes
-  has_many :liked_users, through: :likes, source: :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user, dependent: :destroy
 end
